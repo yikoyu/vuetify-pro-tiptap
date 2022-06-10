6eb386384f7477eef801826bcc376ead5170ae01
yikoyu <2282373181@qq.com>
2022-06-10 15:09:57 +0800

    fix(vuetify): 打开dialog rtl还原问题

--- src/utils/vuetify-instance.ts
+++ src/utils/vuetify-instance.ts
@@ -2,8 +2,22 @@ import Vuetify from 'vuetify/lib'
 import type { VuetifyPreset } from 'vuetify/types'
 
 let vuetifyInstance: Vuetify
+
+/**
+ * @description 修复在 rtl 下调用函数式 dialog, rtl 会还原
+ * @param {Vuetify} instance
+ * @return {*}  {Vuetify}
+ */
+function fixRTL(instance: Vuetify): Vuetify {
+  if (typeof instance.framework.rtl === 'boolean') {
+    instance.preset.rtl = instance.framework.rtl
+  }
+
+  return instance
+}
+
 export function getVuetifyInstance(instance?: Vuetify, preset?: VuetifyPreset) {
-  if (vuetifyInstance) return vuetifyInstance
+  if (vuetifyInstance) return fixRTL(vuetifyInstance)
 
   if (instance) {
     return (vuetifyInstance = instance)
