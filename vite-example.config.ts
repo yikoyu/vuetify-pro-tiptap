10dde50e8976793306e36b0d5111777598aff5fd
滑稽 <2282373181@qq.com>
2022-07-17 17:17:26 +0800

    config(vite): lib和example配置分开

---
+++ vite-example.config.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import eslintPlugin from 'vite-plugin-eslint'
+
+import Components from 'unplugin-vue-components/vite'
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    createVuePlugin(),
+    Components({
+      dirs: undefined,
+      dts: false,
+      resolvers: [VuetifyResolver()]
+    }),
+    eslintPlugin({
+      fix: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { resolve } from 'path'
-import { defineConfig, UserConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
 import eslintPlugin from 'vite-plugin-eslint'
 import dts from 'vite-plugin-dts'
@@ -9,61 +8,33 @@ import Components from 'unplugin-vue-components/vite'
 import { VuetifyResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isExamples = mode === 'examples'
-
-  const config: UserConfig = {
-    base: './',
-    server: {
-      host: '0.0.0.0',
-      port: 3001
-    },
-    plugins: [
-      createVuePlugin({
-        jsx: true
-      }),
-      Components({
-        dirs: undefined,
-        dts: false,
-        resolvers: [VuetifyResolver()]
-      }),
-      eslintPlugin({
-        fix: true
-      })
-    ],
-    optimizeDeps: {
-      include: ['vue', '@vue/composition-api', 'vuetify', 'vuetify/lib']
-    },
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src')
-      }
+export default defineConfig({
+  plugins: [
+    createVuePlugin({
+      jsx: true
+    }),
+    Components({
+      dirs: undefined,
+      dts: false,
+      resolvers: [VuetifyResolver()]
+    }),
+    eslintPlugin({
+      fix: true
+    }),
+    dts({
+      tsConfigFilePath: 'tsconfig.lib.json',
+      insertTypesEntry: true
+    })
+  ],
+  optimizeDeps: {
+    include: ['vue', '@vue/composition-api', 'vuetify', 'vuetify/lib']
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
     }
-  }
-
-  if (isExamples) {
-    const build: UserConfig['build'] = {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            const chunks = [
-              { test: /[\\/]node_modules[\\/]_?vue\//, name: 'chunk-vue' },
-              { test: /[\\/]node_modules[\\/]_?vuetify(.*)/, name: 'chunk-vuetify' }
-            ]
-
-            const chunk = chunks.find(({ test }) => test.test(id))
-            if (chunk) return chunk.name
-          }
-        }
-      }
-    }
-
-    config.build = build
-
-    return config
-  }
-
-  const build: UserConfig['build'] = {
+  },
+  build: {
     outDir: 'lib',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -81,14 +52,4 @@ export default defineConfig(({ mode }) => {
       external: ['vue', '@vue/composition-api', 'vuetify', 'vuetify/lib']
     }
   }
-
-  const dtsPlugin = dts({
-    tsConfigFilePath: 'tsconfig.lib.json',
-    insertTypesEntry: true
-  })
-
-  config.plugins?.push(dtsPlugin)
-  config.build = build
-
-  return config
 })
