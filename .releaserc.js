e6613b6064d53d5e2bc087ac7730b216d0ca90cd
滑稽 <2282373181@qq.com>
2022-07-20 21:09:09 +0800

    config(semantic-release): update release-notes-generator config

--- .releaserc.js
+++ .releaserc.js
@@ -1,15 +1,92 @@
 module.exports = {
   branches: ['master'], // 指定在哪个分支下要执行发布操作
   plugins: [
-    '@semantic-release/commit-analyzer', // 解析 commit 信息，默认就是 Angular 规范
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        "releaseRules": [
+          { breaking: true,     release: "major" },
+          { revert: true,       release: "patch" },
+          { type: "feat",       release: "minor" },
+          { type: "fix",        release: "patch" },
+          { type: "perf",       release: "patch" },
+          { type: "refactor",   release: "patch" },
+          { type: "test",       release: "patch" },
+          { type: "revert",     release: "patch" }
+        ]
+      }
+    ], // 解析 commit 信息，默认就是 Angular 规范
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [{
+              type: "feat",
+              section: "Features",
+              hidden: false
+            },
+            {
+              type: "fix",
+              section: "Bug Fixes",
+              hidden: false
+            },
+            {
+              type: "perf",
+              section: "Performance",
+              hidden: false
+            },
+            {
+              type: "refactor",
+              section: "Refactor",
+              hidden: false
+            },
+            {
+              type: "test",
+              section: "Tests",
+              hidden: false
+            },
+            {
+              type: "revert",
+              section: "Revert",
+              hidden: false
+            },
+            {
+              type: "docs",
+              section: "Docs",
+              hidden: true
+            },
+            {
+              type: "style",
+              section: "Styles",
+              hidden: true
+            },
+            {
+              type: "build",
+              section: "Build",
+              hidden: true
+            },
+            {
+              type: "ci",
+              section: "CI/CD",
+              hidden: true
+            }
+          ]
+        }
+      }
+    ],
     [
       '@semantic-release/changelog',
       {
         changelogFile: 'CHANGELOG.md' // 把发布日志写入该文件
       }
     ],
-    '@semantic-release/npm',
+    [
+      '@semantic-release/npm',
+      // {
+      //   npmPublish: false
+      // }
+    ],
     '@semantic-release/github',
     [
       '@semantic-release/git',
