d33c67a73da2eb07be79ad557c539772c05d1cce
yikoyu <2282373181@qq.com>
2024-01-19 20:31:24 +0800

    fix(Table): unable to create table for mobile devices
    Creating tables on mobile devices with hover is not possible; adding support for click
    
    fix #255

--- src/extensions/components/table/CreateTablePopover.vue
+++ src/extensions/components/table/CreateTablePopover.vue
@@ -3,6 +3,7 @@ import { reactive, ref, unref } from 'vue'
 
 import { TABLE_DEFAULT_SELECTED_GRID_SIZE, TABLE_INIT_GRID_SIZE, TABLE_MAX_GRID_SIZE } from '@/constants/define'
 import { useLocale } from '@/locales'
+import { isMobile } from '@/utils/is-mobile'
 
 export interface GridSize {
   rows: number
@@ -24,8 +25,8 @@ const { t } = useLocale()
 const menu = ref<boolean>(false)
 const withHeaderRow = ref<boolean>(true)
 const tableGridSize = reactive<GridSize>({
-  rows: TABLE_INIT_GRID_SIZE,
-  cols: TABLE_INIT_GRID_SIZE
+  rows: isMobile() ? TABLE_MAX_GRID_SIZE : TABLE_INIT_GRID_SIZE,
+  cols: isMobile() ? TABLE_MAX_GRID_SIZE : TABLE_INIT_GRID_SIZE
 })
 
 const selectedTableGridSize: GridSize = reactive<GridSize>({
@@ -64,7 +65,14 @@ function resetTableGridSize(): void {
 </script>
 
 <template>
-  <VMenu v-model="menu" location="end bottom" open-on-hover :close-on-content-click="false" activator="parent">
+  <VMenu
+    v-model="menu"
+    location="end bottom"
+    open-on-click
+    open-on-hover
+    :close-on-content-click="false"
+    activator="parent"
+  >
     <VCard density="compact" class="table-grid-size-editor">
       <VCardText class="pa-2 pb-0">
         <VCheckbox
