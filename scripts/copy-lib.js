89ad9b503e32ff28590a05419b0fb433fc50b332
Daniel <contact@danni.cool>
2025-01-22 15:20:58 +0800

    feat(dependencies): add fs-extra and nodemon, remove unused fs-extra types

--- scripts/copy-lib.js
+++ scripts/copy-lib.js
@@ -1,14 +1,11 @@
-import { resolve } from 'path'
-import process from 'process'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 import fs from 'fs-extra'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
 const src = resolve(__dirname, '../lib')
 const dest = resolve(__dirname, '../../x/ui/vuetifyx/vuetifyxjs/src/lib/TiptapEditor/lib')
 
-if (process.env.NODE_ENV === 'development') {
   fs.removeSync(dest)
   fs.copySync(src, dest)
   console.log(`Copied lib to ${dest}`)
-} else {
-  console.log('Skipping lib copy in non-development environment')
-}
