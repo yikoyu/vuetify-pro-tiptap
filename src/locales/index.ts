82178c49376ce99b012ef75d4021ff958ff4a8ae
Joseph Levarato <jd.levarato@gmail.com>
2025-05-28 01:10:19 +0200

    feat(locales): :sparkles: added french language support

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -7,6 +7,7 @@ import mitt, { EventType } from '@/utils/mitt'
 // Import language files
 import de from './de'
 import en from './en'
+import fr from './fr'
 import it from './it'
 import nl from './nl'
 import pt from './pt'
@@ -48,6 +49,7 @@ export const DEFAULT_LOCALE_MESSAGE: LangMessages = {
   zhHans,
   en,
   de,
+  fr,
   it,
   pt
 }
