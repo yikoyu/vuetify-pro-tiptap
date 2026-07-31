12390176e9a9f63a1387b917de1f21939c540d8b
yikoyu <2282373181@qq.com>
2026-07-31 23:31:13 +0800

    ci(release): add 2.8.x branch to semantic-release branches

--- release.config.mjs
+++ release.config.mjs
@@ -1,5 +1,5 @@
 import { yikoyu } from '@yikoyu/semantic-release-config'
 
 export default yikoyu({
-  branches: ['master'], // 指定在哪个分支下要执行发布操作
+  branches: ['master', { name: '2.8.x', range: '2.8.x', channel: 'v2-latest' }], // 指定在哪个分支下要执行发布操作
 })
